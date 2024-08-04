ad45bf41d8b9d784dd8eafe3cbe1b3741db5459e
Ansh Gupta <hig6063@gmail.com>
2024-08-05 02:29:37 +0530

    Add files via upload

---
+++ frontend/src/constants.js
@@ -0,0 +1,6 @@
+export const Constants = {
+    API_URL: 'http://127.0.0.1:5000/stream_search',
+    //API_URL: 'https://d21l5c617zttgr.cloudfront.net/stream_search',
+    GITHUB_LINK: "https://github.com/philfung/Senna",
+    JSON_STREAM_SEPARATOR:'[/Senna-SEPARATOR]'
+};
