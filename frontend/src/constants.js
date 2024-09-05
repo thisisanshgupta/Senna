f0e3cbd99de0185f2b48b7039d83a06bcfcb6f30
Ansh Gupta <hig6063@gmail.com>
2024-09-05 20:11:20 +0530

    Delete frontend directory

--- frontend/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const Constants = {
-    API_URL: 'http://127.0.0.1:5000/stream_search',
-    GITHUB_LINK: "https://github.com/thisisanshgupta/Senna",
-    JSON_STREAM_SEPARATOR:'[/Senna-SEPARATOR]'
-};
